a387a8526e60ba51289606bb36700b0625655eb1
Esteban <esgaanza98@gmail.com>
2021-11-14 01:07:07 +1100

    Challenge almost done

---
+++ Computer Vision/Challenge/Computer Vision/jsh.py
@@ -0,0 +1,12 @@
+
+
+import tensorflow as tf
+
+import sys
+
+sys.path.append(
+    "/home/esteban/git/Tensorflow/models/research/object_detection")
+from google.protobuf import text_format
+from object_detection.protos import pipeline_pb2
+from object_detection.utils import config_util
+print("123")
